c55201dd3dca7d2c5ac6af83af883009f453f384
Hunter Morris <hunter.morris@smarkets.com>
2012-03-15 11:51:38 +0000

    Add separate gen_server calls for begin/commit/rollback

--- src/epgsql_connpool_conn.erl
+++ src/epgsql_connpool_conn.erl
@@ -7,6 +7,9 @@
 -export([connection/1, release/1, close/1, transaction/4, dirty/4]).
 -export([start_link/1, init/1, code_change/3, terminate/2,
          handle_call/3, handle_cast/2, handle_info/2]).
+-export([begin_transaction/1,
+         commit_transaction/1,
+         rollback_transaction/1]).
 
 -record(state, {pool, pid}).
 
@@ -34,6 +37,12 @@ dirty(Name, F, Args, Timeout) ->
             after 0 -> {error, transaction_timeout}
             end
     end.
+begin_transaction(Name) ->
+    gen_server:call(Name, begin_transaction, infinity).
+commit_transaction(Name) ->
+    gen_server:call(Name, commit_transaction, infinity).
+rollback_transaction(Name) ->
+    gen_server:call(Name, rollback_transaction, infinity).
 
 init(Name) ->
     case epgsql_connpool_config:conn_by_name(Name) of
@@ -51,17 +60,26 @@ init(Name) ->
 
 terminate(shutdown, #state{pid = P}) -> pgsql:close(P).
 
+handle_call(begin_transaction, _From, #state{pid = P} = State) ->
+    ok = begin_t(P),
+    {reply, {ok, P}, State};
+handle_call(commit_transaction, _From, #state{pid = P} = State) ->
+    ok = commit_t(P),
+    {reply, ok, State};
+handle_call(rollback_transaction, _From, #state{pid = P} = State) ->
+    ok = rollback_t(P),
+    {reply, ok, State};
 handle_call({transaction, F, Args0}, _From, #state{pid = P} = State) ->
-    try {ok, [], []} = pgsql:squery(P, "BEGIN"),
+    try ok = begin_t(P),
          R = apply(F, [P|Args0]),
-         {ok, [], []} = pgsql:squery(P, "COMMIT"),
+         ok = commit_t(P),
          %% XXX: What if exit signal is received here? Response may be lost.
          {reply, {ok, R}, State}
     catch
         throw:{error, closed} ->
             {stop, closed, {error, closed}, State};
         Type:Reason ->
-            {ok, [], []} = pgsql:squery(P, "ROLLBACK"),
+            ok = rollback_t(P),
             {reply, {error, {rollback, Type, Reason}}, State}
     end;
 handle_call({dirty, F, Args0}, _From, #state{pid = P} = State) ->
@@ -81,3 +99,15 @@ handle_info(Msg, _) -> exit({unknown_info, Msg}).
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
 do_release(Name) -> epgsql_connpool:available(Name, self()).
+
+begin_t(Pid) ->
+    {ok, [], []} = pgsql:squery(Pid, "BEGIN"),
+    ok.
+
+commit_t(Pid) ->
+    {ok, [], []} = pgsql:squery(Pid, "COMMIT"),
+    ok.
+
+rollback_t(Pid) ->
+    {ok, [], []} = pgsql:squery(Pid, "ROLLBACK"),
+    ok.
